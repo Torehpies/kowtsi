49f9fd20a499765b5c57efca9040f32006c20a67
Ezedrex <ezzbenig24@gmail.com>
2023-04-28 21:19:11 +0800

    updated profpage

--- Profpg.js
+++ Profpg.js
@@ -1,4 +1,5 @@
 function shata() {
     document.getElementById('navbutton').classList.toggle('active');
     document.getElementById('urnav').classList.toggle('active');
+    document.getElementById('logo').classList.toggle('active');
 }
