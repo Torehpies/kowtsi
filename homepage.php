6b681b87005c90f92358c2e315eeafd9df83c3bc
Ezedrex <ezzbenig24@gmail.com>
2023-05-12 17:03:54 +0800

    more design in homepage

--- homepage.php
+++ homepage.php
@@ -29,6 +29,9 @@
     <link rel="shortcut icon" href="img/kowtsi_logo.ico" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600&family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
--- posting.php
+++ posting.php
@@ -10,12 +10,15 @@
     while ($row = mysqli_fetch_assoc($result))
     {
         echo "<div class = 'test'>";
-        echo "<h2 class = 'author'>" . $row['userID'] . $row['dateAndTime'] . "</h2>";
-        echo "<h2>" . $row['text'] . "</h2>";
+        echo "<div class = 'athr_contain'>";
+        echo "<h2 class = 'author'>" . $row['userID'] . "</h2>";
+        echo "</div>";
+        echo "<p class = 'datentime'>" . $row['dateAndTime'] . "</p>";
+        echo "<h2 class = 'text'>" . $row['text'] . "</h2>";
 
         echo '<form action = "server.php" method = "post" id = "Upvote/Downvote">';
-        echo '<button type = "submit" name =' . $row['postID'] . 'upvote' . 'id = "upvote">' . '<span class = "material-symbols-outlined">' . 'thumb_up' . '</button>' . '</span>' . $row['upvote'];
-        echo '<button type = "submit" name =' . $row['postID'] . 'downvote' . 'id = "upvote">' . '<span class = "material-symbols-outlined">' . 'thumb_down' . '</button>' . '</span>' . $row['downvote'];
+        echo '<button type = "submit" name =' . $row['postID'] . 'upvote' . 'id = "upvote" class = "up_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_up' . '</button>' . '</span>' . $row['upvote'];
+        echo '<button type = "submit" name =' . $row['postID'] . 'downvote' . 'id = "downvote" class = "down_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_down' . '</button>' . '</span>' . $row['downvote'];
         echo '</form>';
 
         echo "</div>";
