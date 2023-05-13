e7649cdd9f47afa5a969e9514123e18092387535
Genrey <genreycristobal99@gmail.com>
2023-05-13 11:10:34 +0800

    Fixed profile page showing errors

--- profile.php
+++ profile.php
@@ -110,56 +110,44 @@
     </div>
       <div id = postsection>
       <div id = "posting">
+
         <?php
-          // DBMS connection code -> hostname, username, password, database name
-          $db = mysqli_connect('localhost', 'root', '', 'kowtsi_db');
 
-          
+        $username = $_SESSION['userID'];
 
-          //Kinukuha yung result sa database
-          $result = mysqli_query($db, "SELECT * FROM quotes WHERE userID = '" . $_SESSION['userID'] . "' ORDER BY dateAndTime DESC");
+        // DBMS connection code -> hostname, username, password, database name
+        $db = mysqli_connect('localhost', 'root', '', 'kowtsi_db');
 
-          if ($result -> num_rows == 0)
-          {
-            echo "<div class = 'test'>";
-            echo "<div class = 'athr_contain'>";
-            echo "<h2 class = 'author'>" . $_SESSION['username'] . "</h2>";
-            echo "<p class = 'datentime'>" . "No posts yet" . "</p>";
-            echo "</div>";
-            echo "<h2 class = 'text'>" . "No posts yet" . "</h2>";
-          }
-
-          $query = "SELECT * FROM quotes WHERE userID = ' $_SESSION['userID'] 'ORDER BY dateAndTime DESC;
-                    SELECT quotes.postID, quotes.text, quotes.userID, quotes. dateAndTime, COUNT(CASE WHEN liketable.like_status = 'like' THEN 1 END) 
-                    AS like_count, COUNT(CASE WHEN liketable.like_status = 'dislike' THEN 1 END) AS dislike_count;";
-
-
-          $result1 = mysqli_query($db, "SELECT quotes.postID, quotes.text, quotes.userID, quotes. dateAndTime, COUNT(CASE WHEN liketable.like_status = 'like' THEN 1 END) AS like_count, COUNT(CASE WHEN liketable.like_status = 'dislike' THEN 1 END) AS dislike_count
+        //Kinukuha yung result sa database
+        $result = mysqli_query($db, "SELECT quotes.postID, quotes.text, quotes.userID, quotes.dateAndTime, COUNT(CASE WHEN liketable.like_status = 'like' THEN 1 END) AS like_count, COUNT(CASE WHEN liketable.like_status = 'dislike' THEN 1 END) AS dislike_count
           FROM quotes
           LEFT JOIN `liketable` ON quotes.postID = liketable.postID
+          WHERE quotes.userID = '$username'
           GROUP BY quotes.postID, quotes.text, quotes.userID, quotes. dateAndTime
           ORDER BY dateAndTime DESC;");
 
-          //Kinukuha ung bawat row at nireresult based dun sa nakuha
-          while ($row = mysqli_fetch_assoc($result))
-          {
-              echo "<div class = 'test'>";
-              echo "<div class = 'athr_contain'>";
-              echo "<h2 class = 'author'>" . $row['userID'] . "</h2>";
-              echo "<p class = 'datentime'>" . $row['dateAndTime'] . "</p>";
-              echo "</div>";
-              echo "<h2 class = 'text'>" . $row['text'] . "</h2>";
+        //Kinukuha ung bawat row at nireresult based dun sa nakuha
+        while ($row = mysqli_fetch_assoc($result))
+        {
+            echo "<div class = 'test'>";
+            echo "<div class = 'athr_contain'>";
+            echo "<h2 class = 'author'>" . $row['userID'] . "</h2>";
+            echo "<p class = 'datentime'>" . $row['dateAndTime'] . "</p>";
+            echo "</div>";
+            echo "<h2 class = 'text'>" . $row['text'] . "</h2>";
 
-              echo '<div class = "likeanddis_contain">';
-              echo '<form action = "server.php" method = "post" id = "Upvote/Downvote">';
-              echo $row['like_count'] . '<button type = "submit" name =' . $row['postID'] . 'upvote' . ' class = "up_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_up' . '</button>' . '</span>';
-              echo '<button type = "submit" name =' . $row['postID'] . 'downvote' . ' class = "down_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_down' . '</button>' . '</span>' . $row['dislike_count'];
-              echo '</form>';
-              echo '</div>';
-              echo "</div>"; 
-          }
+            echo '<div class = "likeanddis_contain">';
+            echo '<form action = "server.php" method = "post" id = "Upvote/Downvote">';
+            echo $row['like_count'] . '<button type = "submit" name =' . $row['postID'] . 'upvote' . ' class = "up_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_up' . '</button>' . '</span>';
+            echo '<button type = "submit" name =' . $row['postID'] . 'downvote' . ' class = "down_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_down' . '</button>' . '</span>' . $row['dislike_count'];
+            echo '</form>';
+            echo '</div>';
+            echo "</div>";
 
-        ?>
+        }
+
+
+      ?>
 
       </div>
     </div>
