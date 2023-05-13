4d42b339571aff9878e821b459e08f219e7ca7e0
Genrey <genreycristobal99@gmail.com>
2023-05-13 10:33:21 +0800

    Merge conflcit

--- profile.php
+++ profile.php
@@ -115,8 +115,10 @@
           // DBMS connection code -> hostname, username, password, database name
           $db = mysqli_connect('localhost', 'root', '', 'kowtsi_db');
 
+          
+
           //Kinukuha yung result sa database
-          $result = mysqli_query($db, "SELECT * FROM quotes WHERE userID = '" . $_SESSION['username'] . "' ORDER BY dateAndTime DESC");
+          $result = mysqli_query($db, "SELECT * FROM quotes WHERE userID = '" . $_SESSION['userID'] . "' ORDER BY dateAndTime DESC");
 
           if ($result -> num_rows == 0)
           {
@@ -127,6 +129,17 @@
             echo "</div>";
             echo "<h2 class = 'text'>" . "No posts yet" . "</h2>";
           }
+
+          $query = "SELECT * FROM quotes WHERE userID = ' $_SESSION['userID'] 'ORDER BY dateAndTime DESC;
+                    SELECT quotes.postID, quotes.text, quotes.userID, quotes. dateAndTime, COUNT(CASE WHEN liketable.like_status = 'like' THEN 1 END) 
+                    AS like_count, COUNT(CASE WHEN liketable.like_status = 'dislike' THEN 1 END) AS dislike_count;";
+
+
+          $result1 = mysqli_query($db, "SELECT quotes.postID, quotes.text, quotes.userID, quotes. dateAndTime, COUNT(CASE WHEN liketable.like_status = 'like' THEN 1 END) AS like_count, COUNT(CASE WHEN liketable.like_status = 'dislike' THEN 1 END) AS dislike_count
+          FROM quotes
+          LEFT JOIN `liketable` ON quotes.postID = liketable.postID
+          GROUP BY quotes.postID, quotes.text, quotes.userID, quotes. dateAndTime
+          ORDER BY dateAndTime DESC;");
 
           //Kinukuha ung bawat row at nireresult based dun sa nakuha
           while ($row = mysqli_fetch_assoc($result))
@@ -140,11 +153,11 @@
 
               echo '<div class = "likeanddis_contain">';
               echo '<form action = "server.php" method = "post" id = "Upvote/Downvote">';
-              echo $row['upvote'] . '<button type = "submit" name =' . $row['postID'] . 'upvote' . 'id = "upvote" class = "up_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_up' . '</button>' . '</span>';
-              echo '<button type = "submit" name =' . $row['postID'] . 'downvote' . 'id = "downvote" class = "down_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_down' . '</button>' . '</span>' . $row['downvote'];
+              echo $row['like_count'] . '<button type = "submit" name =' . $row['postID'] . 'upvote' . ' class = "up_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_up' . '</button>' . '</span>';
+              echo '<button type = "submit" name =' . $row['postID'] . 'downvote' . ' class = "down_vote">' . '<span class = "material-symbols-outlined">' . 'thumb_down' . '</button>' . '</span>' . $row['dislike_count'];
               echo '</form>';
               echo '</div>';
-              echo "</div>";
+              echo "</div>"; 
           }
 
         ?>
